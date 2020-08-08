f0e195e2290ebd5d1f6a5040d8cf2508b0e1235e
Oneted11 <muriithited@gmail.com>
2020-08-09 00:02:32 +0300

    feat:db added

--- api/index.js
+++ api/index.js
@@ -1,12 +1,28 @@
 express = require("express");
+const { MongoClient } = require("mongodb");
 require("dotenv").config();
+
 const app = express();
+
 const {
-    PORT = 3500
-} = process.env
+  PORT = 3500,
 
-app.get("/",(req,res)=>res.send("we are live"));
-app.listen(PORT, () => {
+  DB_URL = "mongodb://localhost:27017",
+} = process.env;
+const client = new MongoClient(DB_URL, { useUnifiedTopology: true });
+const run = async () => {
+  try {
+    await client.connect();
+    const database = client.db("test-book");
+    const collection = database.collection("books");
+    console.log("database connected successfully");
+    server()
+  } catch (error){
+    console.dir(error);
+  }
+};
+run();
+app.get("/", (req, res) => res.send("we are live"));
+const server =()=>{ app.listen(PORT, () => {
   console.log(`the api is running at http://localhost:${PORT}`);
-});
-
+});}
